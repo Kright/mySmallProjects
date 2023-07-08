784b22795eaca9e20c5d018066175ba2cc3e608b
Igor Slobodskov <igor.slobodskov@phystech.edu>
2023-07-08 22:27:07 +0200

    try to use tensorboard

---
+++ 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class PixelNorm2d(nn.Module):
+    """
+    Normalizes each pixel independently, so for each pixel x^2 = 1.
+    """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x / (((x ** 2).sum(dim=1, keepdims=True) + 1e-8) / x.size(1)).sqrt()
