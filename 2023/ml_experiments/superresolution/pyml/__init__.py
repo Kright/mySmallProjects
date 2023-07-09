d82be62a924329b29c8fbfea4c8c20f4c800a89e
Igor Slobodskov <igor.slobodskov@phystech.edu>
2023-07-09 12:31:24 +0200

    add helper class for datasets

--- 2023/ml_experiments/superresolution/pyml/__init__.py
+++ 2023/ml_experiments/superresolution/pyml/__init__.py
@@ -1,3 +1,4 @@
 from .image import compare_shape, assert_shape, get_extension, is_image, image_get_model, downsample_with_gamma
 from .image_saver import ImageSaver
 from .image_dataset import ImagesDataset
+from .datasets import Datasets, Dataset
---
+++ 2023/ml_experiments/superresolution/pyml/datasets.py
@@ -0,0 +1,32 @@
+from typing import List
+from torch.utils.data import DataLoader
+
+from .image_dataset import ImagesDataset
+
+
+class Dataset:
+    def __init__(self, roots: List[str], batch_size: int):
+        self.roots = roots
+        self.images_dataset: ImagesDataset = ImagesDataset.from_dirs_recursive(
+            roots,
+            shuffle_seed=12,
+            channels_order='chw'
+        )
+        self.cuda_loader = self.make_cuda_loader(batch_size)
+
+    def make_cuda_loader(self, batch_size: int) -> DataLoader:
+        return DataLoader(
+            self.images_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            pin_memory=True,
+            pin_memory_device='cuda'
+        )
+
+
+class Datasets:
+    def __init__(self):
+        self.flowers = Dataset(roots=["datasets/flowers102processed"],
+                               batch_size=1)
+        self.squares_all = Dataset(roots=["datasets/srgan256_v0", "datasets/srgan256_v1"],
+                                   batch_size=1)
