8ce19c252da62f48f04fdcd47da5aa52e570a252
Igor Slobodskov <igor.slobodskov@phystech.edu>
2023-07-19 18:18:27 +0200

    change sqrt to rsqrt

--- 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
+++ 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
@@ -7,4 +7,4 @@ class PixelNorm2d(nn.Module):
     Normalizes each pixel independently, so for each pixel x^2 = 1.
     """
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x / (((x ** 2).sum(dim=1, keepdims=True) + 1e-8) / x.size(1)).sqrt()
+        return x * (((x ** 2).sum(dim=1, keepdims=True) + 1e-8) / x.size(1)).rsqrt()
