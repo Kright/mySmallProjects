853a7b64eae4176cf56aa531b387e8820a1ebea8
Igor Slobodskov <igor.slobodskov@phystech.edu>
2023-07-24 11:55:31 +0200

    use keepdim because keepdims doesn't compiles with torch.jit.script

--- 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
+++ 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
@@ -6,6 +6,7 @@ class PixelNorm2d(nn.Module):
     """
     Normalizes each pixel independently, so for each pixel x^2 = 1.
     Same as RMSNorm, but for each pixel independently
+    supports torch.jit.script
     """
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x * (x.pow(2).mean(dim=1, keepdims=True) + 1e-8).rsqrt()
+        return x * (x.pow(2).mean(dim=1, keepdim=True) + 1e-8).rsqrt()
