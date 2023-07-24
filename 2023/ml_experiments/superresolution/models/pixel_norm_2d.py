ca7fc3024fbc503a14009a41b45547d72a0664bd
Igor Slobodskov <igor.slobodskov@phystech.edu>
2023-07-24 11:30:15 +0200

    simplify PixelNorm

--- 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
+++ 2023/ml_experiments/superresolution/models/pixel_norm_2d.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class PixelNorm2d(nn.Module):
     """
     Normalizes each pixel independently, so for each pixel x^2 = 1.
+    Same as RMSNorm, but for each pixel independently
     """
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x * (((x ** 2).sum(dim=1, keepdims=True) + 1e-8) / x.size(1)).rsqrt()
+        return x * (x.pow(2).mean(dim=1, keepdims=True) + 1e-8).rsqrt()
