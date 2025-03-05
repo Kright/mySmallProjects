2d7685b0a0e1857e6f66d45fea58d760a5fee286
Igor Slobodskov <igor.slobodskov@phystech.edu>
2025-03-05 13:37:49 +0100

    f

--- 2025/scalaJmh/src/test/Matrix.scala
+++ 2025/scalaJmh/src/test/Matrix.scala
@@ -49,6 +49,22 @@ class Matrix4x4:
     result
   }
 
+  def multiplyCforFma(m: Matrix4x4): Matrix4x4 = {
+    val result = new Matrix4x4()
+
+    FastRange.cfor(0, _ < h, _ + 1) { y =>
+      FastRange.cfor(0, _ < w, _ + 1) { x =>
+        var sum = 0.0
+        FastRange.cfor(0, _ < w, _ + 1) { k =>
+          sum = Math.fma(this (y, k), m(k, x), sum)
+        }
+        result(y, x) = sum
+      }
+    }
+
+    result
+  }
+
   def multiplyCustomLoop(m: Matrix4x4): Matrix4x4 = {
     val result = new Matrix4x4()
 
